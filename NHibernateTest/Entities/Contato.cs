9bf0a7a43039af682ffddda6fa2fa0afde47053a
Marcio silva <marcioadsworks@gmail.com>
2019-02-18 23:46:52 -0300

    Contato class added

---
+++ NHibernateTest/Entities/Contato.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHibernateTest.Entities
+{
+    public class Contato
+    {
+        public virtual int ID { get; set; }
+        public virtual string Nome { get; set; }
+        public virtual string Telefone { get; set; }
+
+    }
+}
