d2971270f7910a7b914c086a06da665a0395f336
Marcio silva <marcioadsworks@gmail.com>
2019-02-19 00:26:20 -0300

    app1.config changed for app.config

--- NHibernateTest/Form1.cs
+++ NHibernateTest/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NHibernate;
+using NHibernate.Cfg;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,22 @@ namespace NHibernateTest
 {
     public partial class Form1 : Form
     {
+
+        private Configuration myConfiguration;
+        private ISessionFactory mySessionFactory;
+        private ISession mySession;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            myConfiguration = new Configuration();
+            myConfiguration.Configure();
+            mySessionFactory = myConfiguration.BuildSessionFactory();
+            mySession = mySessionFactory.OpenSession();
         }
     }
 }
